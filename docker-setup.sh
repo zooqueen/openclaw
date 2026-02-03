147cbf9d7a85f5ea1f5c125f32246111ec6c967e
Bill Wang <ozbillwang@gmail.com>
2026-02-03 23:16:56 +1100

    feature/OPENCLAW_IMAGE

--- docker-setup.sh
+++ docker-setup.sh
@@ -247,12 +247,17 @@ upsert_env "$ENV_FILE" \
   OPENCLAW_HOME_VOLUME \
   OPENCLAW_DOCKER_APT_PACKAGES
 
-echo "==> Building Docker image: $IMAGE_NAME"
-docker build \
-  --build-arg "OPENCLAW_DOCKER_APT_PACKAGES=${OPENCLAW_DOCKER_APT_PACKAGES}" \
-  -t "$IMAGE_NAME" \
-  -f "$ROOT_DIR/Dockerfile" \
-  "$ROOT_DIR"
+if [ "$IMAGE_NAME" == "openclaw:local" ]; then
+  echo "==> Building Docker image: $IMAGE_NAME"
+  docker build \
+    --build-arg "OPENCLAW_DOCKER_APT_PACKAGES=${OPENCLAW_DOCKER_APT_PACKAGES}" \
+    -t "$IMAGE_NAME" \
+    -f "$ROOT_DIR/Dockerfile" \
+    "$ROOT_DIR"
+else
+  echo "==> Pulling Docker image: $IMAGE_NAME"
+  docker pull "$IMAGE_NAME"
+fi
 
 echo ""
 echo "==> Onboarding (interactive)"
