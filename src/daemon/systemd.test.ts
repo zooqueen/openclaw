4cb9f0b88b81f5c83b09e84df15bb932501321ef
Peter Steinberger <steipete@gmail.com>
2026-02-16 06:27:19 +0000

    test: remove duplicate systemd exec-start split assertion

--- src/daemon/systemd.test.ts
+++ src/daemon/systemd.test.ts
@@ -161,17 +161,6 @@ describe("splitArgsPreservingQuotes", () => {
 });
 
 describe("parseSystemdExecStart", () => {
-  it("splits on whitespace outside quotes", () => {
-    const execStart = "/usr/bin/openclaw gateway start --foo bar";
-    expect(parseSystemdExecStart(execStart)).toEqual([
-      "/usr/bin/openclaw",
-      "gateway",
-      "start",
-      "--foo",
-      "bar",
-    ]);
-  });
-
   it("preserves quoted arguments", () => {
     const execStart = '/usr/bin/openclaw gateway start --name "My Bot"';
     expect(parseSystemdExecStart(execStart)).toEqual([
