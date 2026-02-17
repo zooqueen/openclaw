231ef4cfcb5c5635ecd6ced37d933ad988eadf86
Peter Steinberger <steipete@gmail.com>
2026-02-17 00:32:20 +0000

    style: reformat dedupe-touched files

--- src/agents/tools/web-fetch.ts
+++ src/agents/tools/web-fetch.ts
@@ -1,5 +1,6 @@
 import { Type } from "@sinclair/typebox";
 import type { OpenClawConfig } from "../../config/config.js";
+import type { AnyAgentTool } from "./common.js";
 import { fetchWithSsrFGuard } from "../../infra/net/fetch-guard.js";
 import {
   matchesHostnameAllowlist,
@@ -10,7 +11,6 @@ import { logDebug } from "../../logger.js";
 import { wrapExternalContent, wrapWebContent } from "../../security/external-content.js";
 import { normalizeSecretInput } from "../../utils/normalize-secret-input.js";
 import { stringEnum } from "../schema/typebox.js";
-import type { AnyAgentTool } from "./common.js";
 import { jsonResult, readNumberParam, readStringParam } from "./common.js";
 import {
   extractReadableContent,
