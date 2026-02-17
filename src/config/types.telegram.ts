21483ac03b4ffe7ba18c79eb2cdb47c7a1ceef20
Nimrod Gutman <nimrod.g@singular.net>
2026-02-17 09:44:36 +0200

    Revert "Add Telegram polls action to config typing"
    This reverts commit 5cbfaf5cc70d8d69a39d640eea147d70589eeaaf.

--- src/config/types.telegram.ts
+++ src/config/types.telegram.ts
@@ -14,8 +14,6 @@ import type { GroupToolPolicyBySenderConfig, GroupToolPolicyConfig } from "./typ
 export type TelegramActionConfig = {
   reactions?: boolean;
   sendMessage?: boolean;
-  /** Enable poll actions (send polls). */
-  polls?: boolean;
   deleteMessage?: boolean;
   editMessage?: boolean;
   /** Enable poll actions (sendPoll). */
