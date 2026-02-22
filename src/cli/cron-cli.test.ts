d8dae976a16e7c746d635bc40e073ffb98bead33
Peter Steinberger <steipete@gmail.com>
2026-02-22 00:17:30 +0000

    test(cli): use lightweight clear for cron gateway mock

--- src/cli/cron-cli.test.ts
+++ src/cli/cron-cli.test.ts
@@ -62,7 +62,7 @@ function buildProgram() {
 }
 
 function resetGatewayMock() {
-  callGatewayFromCli.mockReset();
+  callGatewayFromCli.mockClear();
   callGatewayFromCli.mockImplementation(defaultGatewayMock);
 }
 
