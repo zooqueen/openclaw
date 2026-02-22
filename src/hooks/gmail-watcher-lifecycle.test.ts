7ef818c7930c5dbb88ca534297d2cbd2e34d782a
Peter Steinberger <steipete@gmail.com>
2026-02-22 00:27:30 +0000

    test(hooks): use lightweight clears for gmail watcher log spies

--- src/hooks/gmail-watcher-lifecycle.test.ts
+++ src/hooks/gmail-watcher-lifecycle.test.ts
@@ -19,9 +19,9 @@ describe("startGmailWatcherWithLogs", () => {
 
   beforeEach(() => {
     startGmailWatcherMock.mockReset();
-    log.info.mockReset();
-    log.warn.mockReset();
-    log.error.mockReset();
+    log.info.mockClear();
+    log.warn.mockClear();
+    log.error.mockClear();
     delete process.env.OPENCLAW_SKIP_GMAIL_WATCHER;
   });
 
