f1eba751f9dd77cee2ee70a8f0d45bd894cfe02b
Peter Steinberger <steipete@gmail.com>
2026-02-26 04:43:27 +0100

    chore(protocol): regenerate Swift gateway models

--- apps/macos/Sources/OpenClawProtocol/GatewayModels.swift
+++ apps/macos/Sources/OpenClawProtocol/GatewayModels.swift
@@ -2809,6 +2809,7 @@ public struct ExecApprovalsSnapshot: Codable, Sendable {
 public struct ExecApprovalRequestParams: Codable, Sendable {
     public let id: String?
     public let command: String
+    public let commandargv: [String]?
     public let cwd: AnyCodable?
     public let nodeid: AnyCodable?
     public let host: AnyCodable?
@@ -2823,6 +2824,7 @@ public struct ExecApprovalRequestParams: Codable, Sendable {
     public init(
         id: String?,
         command: String,
+        commandargv: [String]?,
         cwd: AnyCodable?,
         nodeid: AnyCodable?,
         host: AnyCodable?,
@@ -2836,6 +2838,7 @@ public struct ExecApprovalRequestParams: Codable, Sendable {
     {
         self.id = id
         self.command = command
+        self.commandargv = commandargv
         self.cwd = cwd
         self.nodeid = nodeid
         self.host = host
@@ -2851,6 +2854,7 @@ public struct ExecApprovalRequestParams: Codable, Sendable {
     private enum CodingKeys: String, CodingKey {
         case id
         case command
+        case commandargv = "commandArgv"
         case cwd
         case nodeid = "nodeId"
         case host
