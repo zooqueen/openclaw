e45cadc054aeb938c79dbaec710a2a0badc8719c
Gustavo Madeira Santana <gumadeiras@gmail.com>
2026-03-04 02:33:02 -0500

    Plugins/lobster: migrate to scoped plugin-sdk imports

--- extensions/lobster/src/windows-spawn.ts
+++ extensions/lobster/src/windows-spawn.ts
@@ -2,7 +2,7 @@ import {
   applyWindowsSpawnProgramPolicy,
   materializeWindowsSpawnProgram,
   resolveWindowsSpawnProgramCandidate,
-} from "openclaw/plugin-sdk/compat";
+} from "openclaw/plugin-sdk/lobster";
 
 type SpawnTarget = {
   command: string;
