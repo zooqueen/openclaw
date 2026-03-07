194c3856c1e946bb753dd26cc4d7487d7742e844
Vincent Koc <vincentkoc@ieee.org>
2026-03-07 15:55:52 -0800

    Gateway Status: allowlist missing token test fixture

--- src/commands/gateway-status.test.ts
+++ src/commands/gateway-status.test.ts
@@ -149,7 +149,7 @@ function makeRemoteGatewayConfig(url: string, token = "rtok", localToken = "ltok
   };
 }
 
-function mockLocalTokenEnvRefConfig(envTokenId = "MISSING_GATEWAY_TOKEN") {
+function mockLocalTokenEnvRefConfig(envTokenId = "MISSING_GATEWAY_TOKEN") { // pragma: allowlist secret
   loadConfig.mockReturnValueOnce({
     secrets: {
       providers: {
