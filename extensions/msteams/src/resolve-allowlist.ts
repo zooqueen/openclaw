ce00fcf9b552ad94af81199668724b2849b3548e
Vincent Koc <vincentkoc@ieee.org>
2026-03-07 16:25:55 -0800

    Teams: use scoped plugin SDK allowlist imports

--- extensions/msteams/src/resolve-allowlist.ts
+++ extensions/msteams/src/resolve-allowlist.ts
@@ -1,4 +1,4 @@
-import { mapAllowlistResolutionInputs } from "openclaw/plugin-sdk";
+import { mapAllowlistResolutionInputs } from "openclaw/plugin-sdk/compat";
 import { searchGraphUsers } from "./graph-users.js";
 import {
   listChannelsForTeam,
