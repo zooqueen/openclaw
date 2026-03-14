6bd02786212f46b452a9519a0593872a6257f85b
Ayaan Zaidi <hi@obviy.us>
2026-03-14 09:44:26 +0530

    build: upload Android native debug symbols

--- apps/android/app/build.gradle.kts
+++ apps/android/app/build.gradle.kts
@@ -80,6 +80,9 @@ android {
             }
             isMinifyEnabled = true
             isShrinkResources = true
+            ndk {
+                debugSymbolLevel = "SYMBOL_TABLE"
+            }
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
         debug {
