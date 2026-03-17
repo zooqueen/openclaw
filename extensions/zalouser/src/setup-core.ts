9957cdfe5ccca3f0445d72f182bd5ba1c54421b4
Peter Steinberger <steipete@gmail.com>
2026-03-17 03:44:10 +0000

    refactor(zalouser): reuse patched setup adapter

--- extensions/zalouser/src/setup-core.ts
+++ extensions/zalouser/src/setup-core.ts
@@ -1,43 +1,9 @@
-import {
-  applyAccountNameToChannelSection,
-  applySetupAccountConfigPatch,
-  DEFAULT_ACCOUNT_ID,
-  migrateBaseNameToDefaultAccount,
-  normalizeAccountId,
-  type ChannelSetupAdapter,
-} from "openclaw/plugin-sdk/setup";
+import { createPatchedAccountSetupAdapter } from "../../../src/channels/plugins/setup-helpers.js";
 
 const channel = "zalouser" as const;
 
-export const zalouserSetupAdapter: ChannelSetupAdapter = {
-  resolveAccountId: ({ accountId }) => normalizeAccountId(accountId),
-  applyAccountName: ({ cfg, accountId, name }) =>
-    applyAccountNameToChannelSection({
-      cfg,
-      channelKey: channel,
-      accountId,
-      name,
-    }),
+export const zalouserSetupAdapter = createPatchedAccountSetupAdapter({
+  channelKey: channel,
   validateInput: () => null,
-  applyAccountConfig: ({ cfg, accountId, input }) => {
-    const namedConfig = applyAccountNameToChannelSection({
-      cfg,
-      channelKey: channel,
-      accountId,
-      name: input.name,
-    });
-    const next =
-      accountId !== DEFAULT_ACCOUNT_ID
-        ? migrateBaseNameToDefaultAccount({
-            cfg: namedConfig,
-            channelKey: channel,
-          })
-        : namedConfig;
-    return applySetupAccountConfigPatch({
-      cfg: next,
-      channelKey: channel,
-      accountId,
-      patch: {},
-    });
-  },
-};
+  buildPatch: () => ({}),
+});
