276e2b2ec81949c8ac47db863b9f000b09b3592a
scoootscooob <167050519+scoootscooob@users.noreply.github.com>
2026-03-18 10:28:59 -0700

    Slack: remove duplicate directory imports (#49935)

--- extensions/slack/src/channel.ts
+++ extensions/slack/src/channel.ts
@@ -38,8 +38,6 @@ import { resolveSlackUserAllowlist } from "./resolve-users.js";
 import {
   buildComputedAccountStatusSnapshot,
   DEFAULT_ACCOUNT_ID,
-  listSlackDirectoryGroupsFromConfig,
-  listSlackDirectoryPeersFromConfig,
   looksLikeSlackTargetId,
   normalizeSlackMessagingTarget,
   PAIRING_APPROVED_MESSAGE,
