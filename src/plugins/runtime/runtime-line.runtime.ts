bd6504251bfba6dc01cb2b0316f21938a429a87f
Peter Steinberger <steipete@gmail.com>
2026-03-22 22:23:16 +0000

    perf: lazy-load plugin runtime heavy surfaces

---
+++ src/plugins/runtime/runtime-line.runtime.ts
@@ -0,0 +1,11 @@
+export { monitorLineProvider } from "../../line/monitor.js";
+export { probeLineBot } from "../../line/probe.js";
+export {
+  pushFlexMessage,
+  pushLocationMessage,
+  pushMessageLine,
+  pushMessagesLine,
+  pushTemplateMessage,
+  pushTextMessageWithQuickReplies,
+  sendMessageLine,
+} from "../../line/send.js";
