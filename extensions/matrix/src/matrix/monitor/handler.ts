c5eef287450a65c27eac53af5dd93f1b6e223a2e
Nick Ludlam <nick@recoil.org>
2026-03-26 22:03:24 +0000

    Fixed testing issue: - We previously triggered expect(getMemberDisplayName).not.toHaveBeenCalled(); - We now only fetch the displayName when the message contains a formatted_body, as we only need to search those messages for mentions using displayName

--- extensions/matrix/src/matrix/monitor/handler.ts
+++ extensions/matrix/src/matrix/monitor/handler.ts
@@ -546,7 +546,9 @@ export function createMatrixRoomMessageHandler(params: MatrixMonitorHandlerParam
         resolveAgentRoute: core.channel.routing.resolveAgentRoute,
       });
       const agentMentionRegexes = core.channel.mentions.buildMentionRegexes(cfg, _route.agentId);
-      const selfDisplayName = await getMemberDisplayName(roomId, selfUserId).catch(() => undefined);
+      const selfDisplayName = content.formatted_body
+        ? await getMemberDisplayName(roomId, selfUserId).catch(() => undefined)
+        : undefined;
       const { wasMentioned, hasExplicitMention } = resolveMentions({
         content,
         userId: selfUserId,
