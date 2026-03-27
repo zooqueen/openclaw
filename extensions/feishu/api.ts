37242afd4c67b13ef3521be3648027bf5db10eac
Peter Steinberger <steipete@gmail.com>
2026-03-27 04:30:46 +0000

    refactor: route session-binding contract tests through public seams

--- extensions/feishu/api.ts
+++ extensions/feishu/api.ts
@@ -2,3 +2,4 @@ export * from "./src/conversation-id.js";
 export * from "./src/setup-core.js";
 export * from "./src/setup-surface.js";
 export * from "./src/thread-bindings.js";
+export { __testing as feishuThreadBindingTesting } from "./src/thread-bindings.js";
