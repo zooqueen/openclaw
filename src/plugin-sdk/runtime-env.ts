d372eba7a21ec2cf34e16418d75dc0a4d56bd297
Peter Steinberger <steipete@gmail.com>
2026-03-28 03:17:12 +0000

    refactor: move discord duration formatting onto runtime env

--- src/plugin-sdk/runtime-env.ts
+++ src/plugin-sdk/runtime-env.ts
@@ -20,7 +20,10 @@ export { isTruthyEnvValue } from "../infra/env.js";
 export * from "../logging.js";
 export { waitForAbortSignal } from "../infra/abort-signal.js";
 export { computeBackoff, sleepWithAbort, type BackoffPolicy } from "../infra/backoff.js";
-export { formatDurationPrecise } from "../infra/format-time/format-duration.ts";
+export {
+  formatDurationPrecise,
+  formatDurationSeconds,
+} from "../infra/format-time/format-duration.ts";
 export { retryAsync } from "../infra/retry.js";
 export { ensureGlobalUndiciEnvProxyDispatcher } from "../infra/net/undici-global-dispatcher.js";
 export { registerUnhandledRejectionHandler } from "../infra/unhandled-rejections.js";
