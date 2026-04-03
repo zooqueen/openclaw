87b792fd1b9f75cd0a44ba535c4db457500f6c56
Vincent Koc <vincentkoc@ieee.org>
2026-04-03 18:46:27 +0900

    fix(plugin-sdk): export ssrf policy type

--- src/plugin-sdk/ssrf-policy.ts
+++ src/plugin-sdk/ssrf-policy.ts
@@ -7,6 +7,7 @@ import {
 } from "../infra/net/ssrf.js";
 
 export { isPrivateIpAddress };
+export type { SsrFPolicy };
 
 export function ssrfPolicyFromAllowPrivateNetwork(
   allowPrivateNetwork: boolean | null | undefined,
