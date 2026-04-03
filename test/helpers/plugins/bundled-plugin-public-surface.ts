94c39d0aeb12576f985a5f49edfee3f42d2cd246
Vincent Koc <vincentkoc@ieee.org>
2026-04-03 23:50:55 +0900

    test(helpers): remove unused bundled plugin surface wrapper

--- test/helpers/plugins/bundled-plugin-public-surface.ts
+++
@@ -1,5 +0,0 @@
-export {
-  loadBundledPluginPublicSurfaceSync,
-  loadBundledPluginTestApiSync,
-  resolveRelativeBundledPluginPublicModuleId,
-} from "../../../src/test-utils/bundled-plugin-public-surface.js";
