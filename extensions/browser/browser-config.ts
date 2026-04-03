7729ddb95c3713c94358ba185d1938deda2bc5fd
Peter Steinberger <steipete@gmail.com>
2026-04-03 12:36:02 +0100

    fix: restore browser-config sdk compatibility

--- extensions/browser/browser-config.ts
+++ extensions/browser/browser-config.ts
@@ -1,11 +1,18 @@
 export {
   DEFAULT_AI_SNAPSHOT_MAX_CHARS,
+  DEFAULT_BROWSER_DEFAULT_PROFILE_NAME,
   DEFAULT_BROWSER_EVALUATE_ENABLED,
   DEFAULT_OPENCLAW_BROWSER_COLOR,
+  DEFAULT_OPENCLAW_BROWSER_ENABLED,
   DEFAULT_OPENCLAW_BROWSER_PROFILE_NAME,
 } from "./src/browser/constants.js";
 export type { ResolvedBrowserConfig, ResolvedBrowserProfile } from "./src/browser/config.js";
-export { resolveBrowserConfig, resolveProfile } from "./src/browser/config.js";
+export {
+  parseHttpUrl as parseBrowserHttpUrl,
+  resolveBrowserConfig,
+  resolveProfile,
+} from "./src/browser/config.js";
 export { redactCdpUrl } from "./src/browser/cdp.helpers.js";
 export { DEFAULT_UPLOAD_DIR } from "./src/browser/paths.js";
+export type { BrowserControlAuth } from "./src/browser/control-auth.js";
 export { resolveBrowserControlAuth } from "./src/browser/control-auth.js";
--- src/plugin-sdk/browser-config.ts
+++ src/plugin-sdk/browser-config.ts
@@ -1,15 +1,19 @@
 export {
   DEFAULT_AI_SNAPSHOT_MAX_CHARS,
+  DEFAULT_BROWSER_DEFAULT_PROFILE_NAME,
   DEFAULT_BROWSER_EVALUATE_ENABLED,
   DEFAULT_OPENCLAW_BROWSER_COLOR,
+  DEFAULT_OPENCLAW_BROWSER_ENABLED,
   DEFAULT_OPENCLAW_BROWSER_PROFILE_NAME,
   DEFAULT_UPLOAD_DIR,
+  parseBrowserHttpUrl,
   redactCdpUrl,
   resolveBrowserConfig,
   resolveBrowserControlAuth,
   resolveProfile,
 } from "../../extensions/browser/browser-config.js";
 export type {
+  BrowserControlAuth,
   ResolvedBrowserConfig,
   ResolvedBrowserProfile,
 } from "../../extensions/browser/browser-config.js";
