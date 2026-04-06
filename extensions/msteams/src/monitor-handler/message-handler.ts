82b055f98d5561891f5444dbcad5c148c794400b
Peter Steinberger <steipete@gmail.com>
2026-04-06 22:51:53 +0100

    refactor: dedupe msteams handler record helper

--- extensions/msteams/src/monitor-handler/message-handler.ts
+++ extensions/msteams/src/monitor-handler/message-handler.ts
@@ -18,6 +18,7 @@ import {
 import {
   buildMSTeamsAttachmentPlaceholder,
   buildMSTeamsMediaPayload,
+  isRecord,
   type MSTeamsAttachmentLike,
   summarizeMSTeamsHtmlAttachments,
 } from "../attachments.js";
@@ -39,9 +40,6 @@ import {
   wasMSTeamsBotMentioned,
 } from "../inbound.js";
 
-function isRecord(value: unknown): value is Record<string, unknown> {
-  return typeof value === "object" && value !== null;
-}
 function extractTextFromHtmlAttachments(attachments: MSTeamsAttachmentLike[]): string {
   for (const attachment of attachments) {
     if (attachment.contentType !== "text/html") {
