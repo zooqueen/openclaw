cf1c23e6a795658094072b2901843aea74c04929
Peter Steinberger <steipete@gmail.com>
2026-04-07 06:32:26 +0100

    fix(test): isolate shared vitest home setup

--- test/setup.ts
+++ test/setup.ts
@@ -1,1 +1,8 @@
-import "./setup.shared.js";
+import { afterAll } from "vitest";
+import { installSharedTestSetup } from "./setup.shared.js";
+
+const testEnv = installSharedTestSetup();
+
+afterAll(() => {
+  testEnv.cleanup();
+});
