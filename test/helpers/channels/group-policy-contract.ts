631ffc4be98ddd089d3461c6924b05a81a1d66a0
Peter Steinberger <steipete@gmail.com>
2026-04-10 09:43:21 +0100

    test: avoid jiti facade load in group policy fallback

--- test/helpers/channels/group-policy-contract.ts
+++ test/helpers/channels/group-policy-contract.ts
@@ -1,15 +1,5 @@
-import { loadBundledPluginTestApiSync } from "../../../src/test-utils/bundled-plugin-public-surface.js";
-
-type WhatsAppTestSurface = typeof import("@openclaw/whatsapp/test-api.js");
-type ZaloTestSurface = typeof import("@openclaw/zalo/test-api.js");
-
-const { resolveWhatsAppRuntimeGroupPolicy } =
-  loadBundledPluginTestApiSync<WhatsAppTestSurface>("whatsapp");
-const { evaluateZaloGroupAccess, resolveZaloRuntimeGroupPolicy } =
-  loadBundledPluginTestApiSync<ZaloTestSurface>("zalo");
-
+export { resolveWhatsAppRuntimeGroupPolicy } from "../../../extensions/whatsapp/src/runtime-group-policy.js";
 export {
   evaluateZaloGroupAccess,
-  resolveWhatsAppRuntimeGroupPolicy,
   resolveZaloRuntimeGroupPolicy,
-};
+} from "../../../extensions/zalo/src/group-access.js";
