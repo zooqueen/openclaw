7453c4a5826780b1d3f45c4e172ddd02c99fcd9a
Peter Steinberger <steipete@gmail.com>
2026-04-23 08:56:47 +0100

    test(whatsapp): coalesce monitor inbox cases

---
+++ extensions/whatsapp/src/monitor-inbox.behavior.test.ts
@@ -0,0 +1,5 @@
+import "./monitor-inbox.allows-messages-from-senders-allowfrom-list.test-support.js";
+import "./monitor-inbox.append-upsert.test-support.js";
+import "./monitor-inbox.blocks-messages-from-unauthorized-senders-not-allowfrom.test-support.js";
+import "./monitor-inbox.captures-media-path-image-messages.test-support.js";
+import "./monitor-inbox.streams-inbound-messages.test-support.js";
