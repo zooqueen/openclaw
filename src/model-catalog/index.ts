23210b9f09ba6b39a2bef76e8d8d5f4721df2c08
Shakker <shakkerdroid@gmail.com>
2026-04-25 03:14:40 +0100

    feat: add model catalog normalizer

---
+++ src/model-catalog/index.ts
@@ -0,0 +1,24 @@
+export {
+  buildModelCatalogMergeKey,
+  buildModelCatalogRef,
+  normalizeModelCatalogProviderId,
+} from "./refs.js";
+export {
+  normalizeModelCatalog,
+  normalizeModelCatalogProviderRows,
+  normalizeModelCatalogRows,
+} from "./normalize.js";
+export type {
+  ModelCatalog,
+  ModelCatalogAlias,
+  ModelCatalogCost,
+  ModelCatalogDiscovery,
+  ModelCatalogInput,
+  ModelCatalogModel,
+  ModelCatalogProvider,
+  ModelCatalogSource,
+  ModelCatalogStatus,
+  ModelCatalogSuppression,
+  ModelCatalogTieredCost,
+  NormalizedModelCatalogRow,
+} from "./types.js";
