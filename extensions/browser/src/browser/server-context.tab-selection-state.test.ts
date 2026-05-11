8e316acafad5693bb2f621a875e2890a76a703f6
Peter Steinberger <steipete@gmail.com>
2026-05-11 21:59:35 +0100

    test: avoid browser tab module reset

--- extensions/browser/src/browser/server-context.tab-selection-state.test.ts
+++ extensions/browser/src/browser/server-context.tab-selection-state.test.ts
@@ -1,11 +1,6 @@
 import { afterEach, describe, expect, it, vi } from "vitest";
 import { withBrowserFetchPreconnect } from "../../test-fetch.js";
 import "../test-support/browser-security.mock.js";
-
-vi.hoisted(() => {
-  vi.resetModules();
-});
-
 import "./server-context.chrome-test-harness.js";
 import { CDP_JSON_NEW_TIMEOUT_MS } from "./cdp-timeouts.js";
 import * as cdpHelpersModule from "./cdp.helpers.js";
