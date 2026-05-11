1b500bfebb9b4d3112a07b1842fdfea970d07339
Peter Steinberger <steipete@gmail.com>
2026-05-11 18:39:43 +0100

    test: use immediate for browser cdp close fixtures

--- extensions/browser/src/browser/chrome.internal.test.ts
+++ extensions/browser/src/browser/chrome.internal.test.ts
@@ -723,7 +723,7 @@ describe("chrome.ts internal", () => {
           wss.on("connection", (ws) => {
             ws.on("message", () => {
               ws.send("not-json-at-all");
-              setTimeout(() => ws.close(), 1);
+              setImmediate(() => ws.close());
             });
           });
         },
@@ -740,7 +740,7 @@ describe("chrome.ts internal", () => {
           wss.on("connection", (ws) => {
             ws.on("message", () => {
               ws.send(JSON.stringify({ id: 42, result: { product: "Chrome" } }));
-              setTimeout(() => ws.close(), 1);
+              setImmediate(() => ws.close());
             });
           });
         },
