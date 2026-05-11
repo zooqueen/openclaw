5ffc53793b9c8c608507a5c5dc854292f291082b
Shakker <shakkerdroid@gmail.com>
2026-05-11 08:25:28 +0100

    test: tighten zca client assertion

--- extensions/zalouser/src/zca-client.test.ts
+++ extensions/zalouser/src/zca-client.test.ts
@@ -17,8 +17,6 @@ describe("zca-client runtime loading", () => {
     const client = await zcaClient.createZalo({ logging: false, selfListen: true });
 
     expect(runtimeFactory).toHaveBeenCalledTimes(1);
-    expect(client).toMatchObject({
-      options: { logging: false, selfListen: true },
-    });
+    expect(client.options).toEqual({ logging: false, selfListen: true });
   });
 });
