c61b256dfbdf41940a9f4ea9e1e8aeb48c3a3804
Peter Steinberger <steipete@gmail.com>
2026-06-01 08:08:09 -0400

    docs: document outbound loader APIs

--- src/channels/plugins/outbound/load.types.ts
+++ src/channels/plugins/outbound/load.types.ts
@@ -1,6 +1,7 @@
 import type { ChannelId } from "../channel-id.types.js";
 import type { ChannelOutboundAdapter } from "../outbound.types.js";
 
+/** Lazy outbound adapter loader contract used by delivery paths. */
 export type LoadChannelOutboundAdapter = (
   id: ChannelId,
 ) => Promise<ChannelOutboundAdapter | undefined>;
