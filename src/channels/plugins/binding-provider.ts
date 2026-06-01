ece814a6105cf1e3bf2dc2363e5812ed1559c940
Peter Steinberger <steipete@gmail.com>
2026-06-01 08:00:10 -0400

    docs: document binding public APIs

--- src/channels/plugins/binding-provider.ts
+++ src/channels/plugins/binding-provider.ts
@@ -1,6 +1,7 @@
 import type { ChannelConfiguredBindingProvider } from "./types.adapters.js";
 import type { ChannelPlugin } from "./types.plugin.js";
 
+/** Returns a plugin's configured-binding provider surface when present. */
 export function resolveChannelConfiguredBindingProvider(
   plugin:
     | Pick<ChannelPlugin, "bindings">
