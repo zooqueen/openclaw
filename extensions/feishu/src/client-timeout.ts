7104c1c2e786f43f42477045844f6ce5a32660c5
Vincent Koc <vincentkoc@ieee.org>
2026-06-17 10:49:28 +0800

    refactor(feishu): hide timeout config type

--- extensions/feishu/src/client-timeout.ts
+++ extensions/feishu/src/client-timeout.ts
@@ -7,7 +7,7 @@ export const FEISHU_HTTP_TIMEOUT_MS = 30_000;
 export const FEISHU_HTTP_TIMEOUT_MAX_MS = 300_000;
 export const FEISHU_HTTP_TIMEOUT_ENV_VAR = "OPENCLAW_FEISHU_HTTP_TIMEOUT_MS";
 
-export type FeishuClientTimeoutConfig = {
+type FeishuClientTimeoutConfig = {
   httpTimeoutMs?: number;
   config?: Pick<FeishuConfig, "httpTimeoutMs">;
 };
