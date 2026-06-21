07cf7a62e1e7e847471af37c4f9552326ea35ad6
Vincent Koc <vincentkoc@ieee.org>
2026-06-21 09:43:10 +0800

    chore(deadcode): drop duplicate script declarations

--- scripts/lib/bundled-plugin-build-entries.d.ts
+++
@@ -1,2 +0,0 @@
-// Bundled Plugin Build Entries.D script supports OpenClaw repository automation.
-export * from "./bundled-plugin-build-entries-types.js";
--- scripts/lib/optional-bundled-clusters.d.ts
+++
@@ -1,2 +0,0 @@
-// Optional Bundled Clusters.D script supports OpenClaw repository automation.
-export * from "./optional-bundled-clusters-types.js";
