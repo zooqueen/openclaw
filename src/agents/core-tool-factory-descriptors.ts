kp-forks/clawdbot

fix(agents): keep exact tool allowlists on owning factories (#104213)
* refactor(agents): centralize core tool factory descriptors

---------

Co-authored-by: Ayaan Zaidi <hi@obviy.us>

src/agents/core-tool-factory-descriptors.ts
@@ -1,0 +1,66 @@
+/**
+ * Static identity for names that select core agent factory families before assembly.
+ */
+
+export type CoreToolFactoryFamily = "base-coding" | "shell" | "openclaw";
+
+type CoreToolFactoryDescriptor = {
+  name: string;
+  family: CoreToolFactoryFamily;
+};
+
+export const CORE_TOOL_FACTORY_DESCRIPTORS = [
+  { name: "edit", family: "base-coding" },
+  { name: "read", family: "base-coding" },
+  { name: "write", family: "base-coding" },
+  { name: "apply_patch", family: "shell" },
+  { name: "exec", family: "shell" },
+  { name: "process", family: "shell" },
+  { name: "agents_list", family: "openclaw" },
+  { name: "crestodian", family: "openclaw" },
+  { name: "computer", family: "openclaw" },
+  { name: "cron", family: "openclaw" },
+  { name: "gateway", family: "openclaw" },
+  { name: "get_goal", family: "openclaw" },
+  { name: "heartbeat_respond", family: "openclaw" },
+  { name: "image", family: "openclaw" },
+  { name: "image_generate", family: "openclaw" },
+  { name: "message", family: "openclaw" },
+  { name: "music_generate", family: "openclaw" },
+  { name: "nodes", family: "openclaw" },
+  { name: "pdf", family: "openclaw" },
+  { name: "session_status", family: "openclaw" },
+  { name: "sessions_history", family: "openclaw" },
+  { name: "sessions_list", family: "openclaw" },
+  { name: "sessions_send", family: "openclaw" },
+  { name: "sessions_spawn", family: "openclaw" },
+  { name: "sessions_yield", family: "openclaw" },
+  { name: "skill_workshop", family: "openclaw" },
+  { name: "spawn_task", family: "openclaw" },
+  { name: "create_goal", family: "openclaw" },
+  { name: "subagents", family: "openclaw" },
+  { name: "transcripts", family: "openclaw" },
+  { name: "tts", family: "openclaw" },
+  { name: "update_goal", family: "openclaw" },
+  { name: "update_plan", family: "openclaw" },
+  { name: "dismiss_task", family: "openclaw" },
+  { name: "video_generate", family: "openclaw" },
+  { name: "web_fetch", family: "openclaw" },
+  { name: "web_search", family: "openclaw" },
+] as const satisfies readonly CoreToolFactoryDescriptor[];
+
+const CORE_TOOL_FACTORY_FAMILY_BY_NAME = new Map<string, CoreToolFactoryFamily>(
+  CORE_TOOL_FACTORY_DESCRIPTORS.map(({ name, family }) => [name, family]),
+);
+
+export type OpenClawCodingToolConstructionPlan = {
+  includeBaseCodingTools: boolean;
+  includeShellTools: boolean;
+  includeChannelTools: boolean;
+  includeOpenClawTools: boolean;
+  includePluginTools: boolean;
+};
+
+export function resolveCoreToolFactoryFamily(name: string): CoreToolFactoryFamily | undefined {
+  return CORE_TOOL_FACTORY_FAMILY_BY_NAME.get(name);
+}

src/agents/embedded-agent-runner/run/attempt-tool-construction-plan.test.ts
@@ -1,5 +1,6 @@
 // Coverage for embedded attempt tool construction and runtime allowlists.
 import { describe, expect, it } from "vitest";
+import { CORE_TOOL_FACTORY_DESCRIPTORS } from "../../core-tool-factory-descriptors.js";
 import {
   applyEmbeddedAttemptToolsAllow,
   mergeForcedEmbeddedAttemptToolsAllow,
@@ -182,6 +183,26 @@ describe("applyEmbeddedAttemptToolsAllow", () => {
 });
 
 describe("resolveEmbeddedAttemptToolConstructionPlan", () => {
+  it("keeps factory descriptor names unique", () => {
+    const names = CORE_TOOL_FACTORY_DESCRIPTORS.map((descriptor) => descriptor.name);
+
+    expect(new Set(names).size).toBe(names.length);
+  });
+
+  it.each(CORE_TOOL_FACTORY_DESCRIPTORS)(
+    "routes $name through its $family factory",
+    ({ name, family }) => {
+      const plan = resolveEmbeddedAttemptToolConstructionPlan({ toolsAllow: [name] });
+
+      expect(plan.codingToolConstructionPlan).toMatchObject({
+        includeBaseCodingTools: family === "base-coding",
+        includeShellTools: family === "shell",
+        includeOpenClawTools: family === "openclaw",
+        includePluginTools: false,
+      });
+    },
+  );
+
   it("builds all tool families when no runtime allowlist is present", () => {
     expectConstructionPlan(resolveEmbeddedAttemptToolConstructionPlan({}), {
       constructTools: true,
@@ -386,7 +407,29 @@ describe("resolveEmbeddedAttemptToolConstructionPlan", () => {
     );
   });
 
+  it("materializes transcripts through the core factory", () => {
+    expectConstructionPlan(
+      resolveEmbeddedAttemptToolConstructionPlan({ toolsAllow: ["transcripts"] }),
+      {
+        includeCoreTools: true,
+        coding: {
+          includeChannelTools: false,
+          includeOpenClawTools: true,
+          includePluginTools: false,
+        },
+      },
+    );
+  });
+
   it("keeps plugin-owned catalog tools on the plugin construction path", () => {
+    expectConstructionPlan(resolveEmbeddedAttemptToolConstructionPlan({ toolsAllow: ["canvas"] }), {
+      includeCoreTools: false,
+      coding: {
+        includeChannelTools: true,
+        includeOpenClawTools: false,
+        includePluginTools: true,
+      },
+    });
     expectConstructionPlan(
       resolveEmbeddedAttemptToolConstructionPlan({ toolsAllow: ["browser"] }),
       {

zooqueen/openclaw

(cherry picked from commit f2a0f00567466475495f00280d965d51185b5645)

src/agents/core-tool-factory-descriptors.ts
@@ -1,0 +1,63 @@
+/**
+ * Static identity for names that select core agent factory families before assembly.
+ */
+
+export type CoreToolFactoryFamily = "base-coding" | "shell" | "openclaw";
+
+type CoreToolFactoryDescriptor = {
+  name: string;
+  family: CoreToolFactoryFamily;
+};
+
+export const CORE_TOOL_FACTORY_DESCRIPTORS = [
+  { name: "edit", family: "base-coding" },
+  { name: "read", family: "base-coding" },
+  { name: "write", family: "base-coding" },
+  { name: "apply_patch", family: "shell" },
+  { name: "exec", family: "shell" },
+  { name: "process", family: "shell" },
+  { name: "agents_list", family: "openclaw" },
+  { name: "canvas", family: "openclaw" },
+  { name: "cron", family: "openclaw" },
+  { name: "gateway", family: "openclaw" },
+  { name: "get_goal", family: "openclaw" },
+  { name: "heartbeat_respond", family: "openclaw" },
+  { name: "heartbeat_response", family: "openclaw" },
+  { name: "image", family: "openclaw" },
+  { name: "image_generate", family: "openclaw" },
+  { name: "message", family: "openclaw" },
+  { name: "music_generate", family: "openclaw" },
+  { name: "nodes", family: "openclaw" },
+  { name: "pdf", family: "openclaw" },
+  { name: "session_status", family: "openclaw" },
+  { name: "sessions_history", family: "openclaw" },
+  { name: "sessions_list", family: "openclaw" },
+  { name: "sessions_send", family: "openclaw" },
+  { name: "sessions_spawn", family: "openclaw" },
+  { name: "sessions_yield", family: "openclaw" },
+  { name: "skill_workshop", family: "openclaw" },
+  { name: "create_goal", family: "openclaw" },
+  { name: "subagents", family: "openclaw" },
+  { name: "tts", family: "openclaw" },
+  { name: "update_goal", family: "openclaw" },
+  { name: "update_plan", family: "openclaw" },
+  { name: "video_generate", family: "openclaw" },
+  { name: "web_fetch", family: "openclaw" },
+  { name: "web_search", family: "openclaw" },
+] as const satisfies readonly CoreToolFactoryDescriptor[];
+
+const CORE_TOOL_FACTORY_FAMILY_BY_NAME = new Map<string, CoreToolFactoryFamily>(
+  CORE_TOOL_FACTORY_DESCRIPTORS.map(({ name, family }) => [name, family]),
+);
+
+export type OpenClawCodingToolConstructionPlan = {
+  includeBaseCodingTools: boolean;
+  includeShellTools: boolean;
+  includeChannelTools: boolean;
+  includeOpenClawTools: boolean;
+  includePluginTools: boolean;
+};
+
+export function resolveCoreToolFactoryFamily(name: string): CoreToolFactoryFamily | undefined {
+  return CORE_TOOL_FACTORY_FAMILY_BY_NAME.get(name);
+}

src/agents/embedded-agent-runner/run/attempt-tool-construction-plan.test.ts
@@ -1,5 +1,6 @@
 // Coverage for embedded attempt tool construction and runtime allowlists.
 import { describe, expect, it } from "vitest";
+import { CORE_TOOL_FACTORY_DESCRIPTORS } from "../../core-tool-factory-descriptors.js";
 import {
   applyEmbeddedAttemptToolsAllow,
   mergeForcedEmbeddedAttemptToolsAllow,
@@ -99,8 +100,9 @@ describe("applyEmbeddedAttemptToolsAllow", () => {
   it("keeps plugin-only allowlists on the shared tool policy path", () => {
     const tools = [{ name: "memory_search" }, { name: "plugin_extra" }];
 
-    expect(resolveEmbeddedAttemptToolConstructionPlan({ toolsAllow: ["memory_search"] }))
-      .toHaveProperty("includeCoreTools", false);
+    expect(
+      resolveEmbeddedAttemptToolConstructionPlan({ toolsAllow: ["memory_search"] }),
+    ).toHaveProperty("includeCoreTools", false);
     expect(
       applyEmbeddedAttemptToolsAllow(tools, ["memory_search"]).map((tool) => tool.name),
     ).toEqual(["memory_search"]);
@@ -181,6 +183,26 @@ describe("applyEmbeddedAttemptToolsAllow", () => {
 });
 
 describe("resolveEmbeddedAttemptToolConstructionPlan", () => {
+  it("keeps factory descriptor names unique", () => {
+    const names = CORE_TOOL_FACTORY_DESCRIPTORS.map((descriptor) => descriptor.name);
+
+    expect(new Set(names).size).toBe(names.length);
+  });
+
+  it.each(CORE_TOOL_FACTORY_DESCRIPTORS)(
+    "routes $name through its $family factory",
+    ({ name, family }) => {
+      const plan = resolveEmbeddedAttemptToolConstructionPlan({ toolsAllow: [name] });
+
+      expect(plan.codingToolConstructionPlan).toMatchObject({
+        includeBaseCodingTools: family === "base-coding",
+        includeShellTools: family === "shell",
+        includeOpenClawTools: family === "openclaw",
+        includePluginTools: false,
+      });
+    },
+  );
+
   it("builds all tool families when no runtime allowlist is present", () => {
     expectConstructionPlan(resolveEmbeddedAttemptToolConstructionPlan({}), {
       constructTools: true,