32b573f6592532ab4756beb59b660a4a2dea61de
Peter Steinberger <steipete@gmail.com>
2026-07-19 13:52:17 +0100

    test(release): track guarded publication order

--- test/scripts/package-acceptance-workflow.test.ts
+++ test/scripts/package-acceptance-workflow.test.ts
@@ -4045,7 +4045,7 @@ describe("package artifact reuse", () => {
       "\n              if promote_windows_release_assets; then\n",
     );
     const publishReleaseCall = releaseWorkflow.lastIndexOf(
-      "\n              publish_github_release\n",
+      "\n                publish_github_release\n",
     );
     expect(createDraftCall).toBeGreaterThan(-1);
     expect(promoteWindowsCall).toBeGreaterThan(createDraftCall);
@@ -4181,7 +4181,7 @@ describe("package artifact reuse", () => {
       "\n              if promote_android_release_asset; then\n",
     );
     const publishReleaseCall = releaseWorkflow.lastIndexOf(
-      "\n              publish_github_release\n",
+      "\n                publish_github_release\n",
     );
     expect(createDraftCall).toBeGreaterThan(-1);
     expect(promoteAndroidCall).toBeGreaterThan(createDraftCall);
